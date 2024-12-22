c068be1a9358aa69edbdce94b7aaca4e653ae66d
Hanze Chen <hanze2010@outlook.com>
2024-12-22 21:23:52 +0800

    Update utils.ts

--- api/utils.ts
+++ api/utils.ts
@@ -39,9 +39,9 @@ export function replaceFileUrl(obj: Record<string, any>) {
     if (typeof val === 'string') {
       if (val.startsWith('https://')) {
         obj[key] = val
-          .replace('storage1.picacomic.com', 's3.picacomic.com')
-          .replace('storage-b.picacomic.com', 's3.picacomic.com')
-          .replace('img.picacomic.com', 's3.picacomic.com')
+          .replace('storage1.picacomic.com', 'proxy.hanze.icu/proxy/s3.picacomic.com')
+          .replace('storage-b.picacomic.com', 'proxy.hanze.icu/proxy/s3.picacomic.com')
+          .replace('img.picacomic.com', 'proxy.hanze.icu/proxy/s3.picacomic.com')
           .replace('www.picacomic.com', 'pica-pica.wikawika.xyz')
       }
     }
